2ec111efb017dc8ce8568f256e4b36946f8bbfaf
cailufei <cailufei@xqshijie.cn>
2018-05-21 16:56:19 +0800

    cailufei

---
+++ ffff/ffff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ffff
+//
+//  Created by 蔡路飞 on 2018/5/21.
+//  Copyright © 2018年 蔡路飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
