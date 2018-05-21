d048a2ac462fbe32be23953007c8476c7f46be43
cailufei <cailufei@xqshijie.cn>
2018-05-21 20:43:32 +0800

    增加View

---
+++ BlockChain/BlockChain/BCClass/BCMe/BCMeView/BCHomeView.m
@@ -0,0 +1,21 @@
+//
+//  BCHomeView.m
+//  BlockChain
+//
+//  Created by 蔡路飞 on 2018/5/21.
+//  Copyright © 2018年 蔡路飞. All rights reserved.
+//
+
+#import "BCHomeView.h"
+
+@implementation BCHomeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
