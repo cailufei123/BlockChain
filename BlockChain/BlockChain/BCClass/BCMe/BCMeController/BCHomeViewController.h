03c59328ac4ddd9aed8bfa2718f8705c5440a8d0
cailufei <cailufei@xqshijie.cn>
2018-05-21 20:31:52 +0800

    初始化项目

---
+++ BlockChain/BlockChain/BCClass/BCMe/BCMeController/BCHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCHomeViewController.h
+//  BlockChain
+//
+//  Created by 蔡路飞 on 2018/5/21.
+//  Copyright © 2018年 蔡路飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCHomeViewController : UIViewController
+
+@end
---
+++ BlockChain/BlockChain/BCClass/BCMe/BCMeController/BCHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  BCHomeViewController.m
+//  BlockChain
+//
+//  Created by 蔡路飞 on 2018/5/21.
+//  Copyright © 2018年 蔡路飞. All rights reserved.
+//
+
+#import "BCHomeViewController.h"
+
+@interface BCHomeViewController ()
+
+@end
+
+@implementation BCHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
