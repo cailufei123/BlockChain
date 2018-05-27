610307fbd3e6e8094d3380c6c1506000613a6d78
wuboyi1234 <925905055@qq.com>
2018-05-27 16:44:41 +0800

    no message

--- BlockChain/BlockChain/BCClass/BCMe/BCMeController/BCMeViewController.m
+++ BlockChain/BlockChain/BCClass/BCMe/BCMeController/BCMeViewController.m
@@ -117,10 +117,10 @@ static NSString * const cellidenfder = @"BCMeTableViewCell";
 -(void)onNavButtonTapped:(UIBarButtonItem *)sender event:(UIEvent *)event
 {
     
-    BCMeInvitingFriendsController *setVc =[[BCMeInvitingFriendsController alloc] init];
-    [self.navigationController pushViewController:setVc animated:YES];
-//    BCSetViewController *setVc =[[BCSetViewController alloc] init];
+//    BCMeInvitingFriendsController *setVc =[[BCMeInvitingFriendsController alloc] init];
 //    [self.navigationController pushViewController:setVc animated:YES];
+    BCSetViewController *setVc =[[BCSetViewController alloc] init];
+    [self.navigationController pushViewController:setVc animated:YES];
 }
 
 #pragma 糖果记录跳转
