41707d8b9ffba2c226c0c854bb9d3c977c8031f4
cailufei <cailufei@xqshijie.cn>
2018-06-22 10:13:34 +0800

    no message

--- BlockChain/BlockChain/BCClass/Other/Public/URL.h
+++ BlockChain/BlockChain/BCClass/Other/Public/URL.h
@@ -25,7 +25,7 @@
 #else
 
 
-#define SERVER_ADDR @"http://welth.leilook.com" //新线上域名
+#define SERVER_ADDR @"http://caifu.leilook.com" //新线上域名
 //#define SERVER_ADDR @"http://esp.im-come.com"
 //#define USER_SERVER_ADDR @"http://esp.im-come.com"
 //#define PAY_SERVER_ADDR  @"http://im-come.com"
